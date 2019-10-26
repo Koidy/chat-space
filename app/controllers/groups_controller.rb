1978963a646dd07c229b5d455ea9b896845a0654
Koidy <yuji.koido@gmail.com>
2019-10-26 14:51:37 +0900

    Update groups_controller.rb
    edit とupdate のアクションは一旦削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,12 +13,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
