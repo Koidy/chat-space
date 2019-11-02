309f19c17cc630b876ca0d96d34fb944fe4570d9
Koidy <yuji.koido@gmail.com>
2019-11-02 21:21:15 +0900

    Update users.js
    done関数の処理を追加

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,20 @@
 $(function() {
+  function addUser(user){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザー名</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id}" data-user-name="${user.name}">追加</div>
+              </div>
+              `
+  }
+
+  function addNoUser(user){
+    var html = `
+              <div class="chat-group-user cleafix">
+                <p class="chat-group-user__name">ユーザーがみつかりません</p>
+              <div>`
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
 
@@ -9,10 +25,20 @@ $(function() {
       data: { keyword: input }
     })
       .done(function(users) {
-        console.log("成功です");
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
       })
       .fail(function() {
-        console.log("失敗です");
+        alert("通信エラーです。ユーザーが表示できません。");
       });
   });
 });
