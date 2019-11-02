58b5e09355ead36145015fc6b188f81ada832634
Koidy <yuji.koido@gmail.com>
2019-11-02 20:11:57 +0900

    Update users_controller.rb
    users_controllerに曖昧な文字列の検索などを追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
   def index
-    resond_to do |format|
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
       format.html
       format.json
     end
