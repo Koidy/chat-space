2620771fe4dec4862de9dda1abc5a06d8ef589b6
Koidy <yuji.koido@gmail.com>
2019-11-04 12:02:10 +0900

    Update users.js
    記述を修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,21 @@
 $(function() {
-  function addUser(user){
-    var html = `
-              <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">ユーザー名</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id}" data-user-name="${user.name}">追加</div>
-              </div>
-              `
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    $("#user-search-result").append(html);
   }
 
-  function addNoUser(user){
-    var html = `
-              <div class="chat-group-user cleafix">
-                <p class="chat-group-user__name">ユーザーがみつかりません</p>
-              <div>`
+  function addNoUser(){
+    let html = `
+      <div class="chat-group-user cleafix">
+        <p class="chat-group-user__name">ユーザーがみつかりません</p>
+      <div>
+    `;
+    $("#user-search-result").append(html);
   }
 
   $("#user-search-field").on("keyup", function() {
