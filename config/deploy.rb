7ec0f2331e89527e0ed645dc547fb651cb74c808
Koidy <yuji.koido@gmail.com>
2019-11-09 12:01:28 +0900

    Update deploy.rb
    パスの記述を訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['.ssh/cspace.pem']
+                  keys: ['~/.ssh/cspace.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
